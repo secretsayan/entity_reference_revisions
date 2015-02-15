979771d21b54455467bff65fdba364a7f630fec7
Jeroen <jeroen@vdmi.nl>
2015-02-15 13:54:49 +0100

    Implemented datatype, now loads proper revision on form

--- src/Plugin/Field/FieldType/EntityReferenceRevisionsItem.php
+++ src/Plugin/Field/FieldType/EntityReferenceRevisionsItem.php
@@ -35,16 +35,55 @@ use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
  *   constraints = {"ValidReference" = {}}
  * )
  */
-class EntityReferenceRevisionsItem extends EntityReferenceItem {
+class EntityReferenceRevisionsItem extends FieldItemBase {
+
+
+  /**
+   * Marker value to identify a newly created entity.
+   *
+   * @var int
+   */
+  protected static $NEW_ENTITY_MARKER = -1;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultStorageSettings() {
+    return array(
+      'target_type' => \Drupal::moduleHandler()->moduleExists('node') ? 'node' : 'user',
+      'target_bundle' => NULL,
+    ) + parent::defaultStorageSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultFieldSettings() {
+    return array(
+      'handler' => 'default',
+    ) + parent::defaultFieldSettings();
+  }
 
   /**
    * {@inheritdoc}
    */
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
-    $properties = parent::propertyDefinitions($field_definition);
-
     $settings = $field_definition->getSettings();
     $target_type_info = \Drupal::entityManager()->getDefinition($settings['target_type']);
+
+    if ($target_type_info->isSubclassOf('\Drupal\Core\Entity\FieldableEntityInterface')) {
+      // @todo: Lookup the entity type's ID data type and use it here.
+      // https://drupal.org/node/2107249
+      $target_id_definition = DataDefinition::create('integer')
+        ->setLabel(t('@label ID', array($target_type_info->getLabel())))
+        ->setSetting('unsigned', TRUE);
+    }
+    else {
+      $target_id_definition = DataDefinition::create('string')
+        ->setLabel(t('@label ID', array($target_type_info->getLabel())));
+    }
+    $target_id_definition->setRequired(TRUE);
+    $properties['target_id'] = $target_id_definition;
 
     if ($target_type_info->getKey('revision')) {
       $target_revision_id_definition = DataDefinition::create('integer')
@@ -55,17 +94,62 @@ class EntityReferenceRevisionsItem extends EntityReferenceItem {
       $properties['target_revision_id'] = $target_revision_id_definition;
     }
 
+    $properties['entity'] = DataReferenceDefinition::create('entity_revision')
+      ->setLabel($target_type_info->getLabel())
+      ->setDescription(t('The referenced entity revision'))
+      // The entity object is computed out of the entity ID.
+      ->setComputed(TRUE)
+      ->setReadOnly(FALSE)
+      ->setTargetDefinition(EntityDataDefinition::create($settings['target_type']));
+
+    if (isset($settings['target_bundle'])) {
+      $properties['entity']->getTargetDefinition()->addConstraint('Bundle', $settings['target_bundle']);
+    }
+
     return $properties;
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function schema(FieldStorageDefinitionInterface $field_definition) {
-    $schema = parent::schema($field_definition);
+  public static function mainPropertyName() {
+    return 'target_id';
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
     $target_type = $field_definition->getSetting('target_type');
     $target_type_info = \Drupal::entityManager()->getDefinition($target_type);
+
+    if ($target_type_info->isSubclassOf('\Drupal\Core\Entity\FieldableEntityInterface')) {
+      $columns = array(
+        'target_id' => array(
+          'description' => 'The ID of the target entity.',
+          'type' => 'int',
+          'unsigned' => TRUE,
+        ),
+      );
+    }
+    else {
+      $columns = array(
+        'target_id' => array(
+          'description' => 'The ID of the target entity.',
+          'type' => 'varchar',
+          // If the target entities act as bundles for another entity type,
+          // their IDs should not exceed the maximum length for bundles.
+          'length' => $target_type_info->getBundleOf() ? EntityTypeInterface::BUNDLE_MAX_LENGTH : 255,
+        ),
+      );
+    }
+
+    $schema = array(
+      'columns' => $columns,
+      'indexes' => array(
+        'target_id' => array('target_id'),
+      ),
+    );
 
     if ($target_type_info->getKey('revision')) {
       $schema['columns']['target_revision_id'] = array(
@@ -90,10 +174,89 @@ class EntityReferenceRevisionsItem extends EntityReferenceItem {
     }
     else {
       parent::setValue($values, FALSE);
-      if (isset($values['target_revision_id']) && !isset($values['entity'])) {
+      // Support setting the field item with only one property, but make sure
+      // values stay in sync if only property is passed.
+      if (isset($values['target_id']) && !isset($values['entity'])) {
+        $this->onChange('target_id', FALSE);
+      }
+      elseif (isset($values['target_revision_id']) && !isset($values['entity'])) {
         $this->onChange('target_revision_id', FALSE);
       }
+      elseif (!isset($values['target_id']) && isset($values['entity'])) {
+        $this->onChange('entity', FALSE);
+      }
+      elseif (isset($values['target_id']) && isset($values['entity'])) {
+        // If both properties are passed, verify the passed values match. The
+        // only exception we allow is when we have a new entity: in this case
+        // its actual id and target_id will be different, due to the new entity
+        // marker.
+        $entity_id = $this->get('entity')->getTargetIdentifier();
+        if ($entity_id != $values['target_id'] && ($values['target_id'] != static::$NEW_ENTITY_MARKER || !$this->entity->isNew())) {
+          throw new \InvalidArgumentException('The target id and entity passed to the entity reference item do not match.');
+        }
+      }
+      // Notify the parent if necessary.
+      if ($notify && $this->parent) {
+        $this->parent->onChange($this->getName());
+      }
     }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValue() {
+    $values = parent::getValue();
+
+    // If there is an unsaved entity, return it as part of the field item values
+    // to ensure idempotency of getValue() / setValue().
+    if ($this->hasNewEntity()) {
+      $values['entity'] = $this->entity;
+    }
+    return $values;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onChange($property_name, $notify = TRUE) {
+    // Make sure that the target ID and the target property stay in sync.
+    if ($property_name == 'entity') {
+      $property = $this->get('entity');
+      $target_id = $property->isTargetNew() ? static::$NEW_ENTITY_MARKER : $property->getTargetIdentifier();
+      $this->writePropertyValue('target_id', $target_id);
+    }
+    elseif ($property_name == 'target_id' && $this->target_id != static::$NEW_ENTITY_MARKER) {
+      $this->writePropertyValue('entity', array(
+        'target_id' => $this->target_id,
+        'target_revision_id' => $this->target_revision_id,
+      ));
+    }
+    elseif ($property_name == 'target_revision_id' && $this->target_revision_id) {
+      $this->writePropertyValue('entity', array(
+        'target_id' => $this->target_id,
+        'target_revision_id' => $this->target_revision_id,
+      ));
+    }
+    parent::onChange($property_name, $notify);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    // Avoid loading the entity by first checking the 'target_id'.
+    if ($this->target_id !== NULL) {
+      return FALSE;
+    }
+    if ($this->target_revision_id !== NULL) {
+      return FALSE;
+    }
+    if ($this->entity && $this->entity instanceof EntityInterface) {
+      return FALSE;
+    }
+    return TRUE;
   }
 
   /**
@@ -158,5 +321,4 @@ class EntityReferenceRevisionsItem extends EntityReferenceItem {
     }
     return $dependencies;
   }
-
 }
